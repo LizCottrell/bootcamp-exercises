558321d007a34120732af02d29b38c4b7d734182
Elizabeth Cottrell <elizabeth@openforge.io>
2019-02-04 22:56:42 -0500

    refactor(word-guess-game) add functionality

--- 03-javascript/word-guess-game/assets/javascript/game.js
+++ 03-javascript/word-guess-game/assets/javascript/game.js
@@ -10,6 +10,8 @@ const words = [
 ];
 
 //===== variables
+//============================
+
 let score = document.querySelector("[data-target='score']");
 let currentWord = document.querySelector("[data-target='currentWord']");
 let guesses = document.querySelector("[data-target='guesses']");
@@ -21,6 +23,8 @@ let letterList = [];
 let word;
 
 //===== functions
+//============================
+
 function startGame() {
   // pick a random word from the list
   word = words[Math.floor(Math.random() * words.length)].toUpperCase();
@@ -38,6 +42,23 @@ function startGame() {
   }
 }
 
+function logGuess() {
+  remainingGuesses--;
+  updateRemainingGuesses();
+  if (remainingGuesses === 0) {
+    wins--;
+    updateScore();
+    letterList = [];
+    updateLetterList();
+    currentWord.innerHTML = "";
+    startGame();
+  }
+}
+
+function updateRemainingGuesses() {
+  guesses.innerHTML = remainingGuesses;
+}
+
 function updateScore() {
   score.innerHTML = wins;
 }
@@ -49,36 +70,52 @@ function updateLetterList(letter) {
   letters.innerHTML = exportedLetterList;
 }
 
-function updateRemainingGuesses() {
-  guesses.innerHTML = remainingGuesses;
+function isAlpha() {
+  var charCode = event.keyCode;
+  if (
+    (charCode > 64 && charCode < 91) ||
+    (charCode > 96 && charCode < 123) ||
+    charCode == 8
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //===== main process
+//============================
 
 startGame();
 updateScore();
 
 document.onkeyup = function(event) {
   let key = event.key.toUpperCase();
-  // if key press is any of the letters, show those letters
-  if (word.indexOf(key) > -1) {
-    // find the indexOf(key)
-    let indices = [];
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === key) indices.push(i);
+
+  // don't run program if letter was already guessed
+  if (letterList.includes(key)) {
+    logGuess();
+  } else if (isAlpha(key)) {
+    // only run program if keypress is a letter
+    // if key press is any of the letters, show those letters
+    if (word.indexOf(key) > -1) {
+      // find the indexOf(key)
+      let indices = [];
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === key) indices.push(i);
+      }
+      // show the relevant span(s)
+      let spans = document.querySelectorAll("[data-target='currentWord'] span");
+      for (let i = 0; i < indices.length; i++) {
+        spans[indices[i]].classList.add("show");
+      }
+      // add key to letter list
+      updateLetterList(key);
+    } else {
+      // add key to letter list
+      updateLetterList(key);
+      // subtract a turn
+      logGuess();
     }
-    // add .show to the relevant span(s)
-    let spans = document.querySelectorAll("[data-target='currentWord'] span");
-    for (let i = 0; i < indices.length; i++) {
-      spans[indices[i]].classList.add("show");
-    }
-    // add key to letter list
-    updateLetterList(key);
-  } else {
-    // add key to letter list
-    updateLetterList(key);
-    // subtract a turn
-    remainingGuesses--;
-    updateRemainingGuesses();
   }
 };
