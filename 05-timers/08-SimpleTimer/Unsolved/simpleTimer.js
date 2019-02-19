3c692396b4ecd152cc00eced1b515924bc1436e9
Elizabeth Cottrell <elizabeth@openforge.io>
2019-02-18 20:20:41 -0500

    05-timers : in-progress

---
+++ 05-timers/08-SimpleTimer/Unsolved/simpleTimer.js
@@ -0,0 +1,43 @@
+// Step 1:
+// Use the following Audio file below:
+
+var audio = new Audio("horn.wav");
+
+//  Step 2: do the following between the --- comments below
+//  after 5 seconds, execute the fiveSeconds function
+//  after 10 seconds, execute the tenSeconds function
+//  after 15 seconds, execute the timeUp function
+
+// ---------------------
+setTimeout(fiveSeconds, 5000);
+setTimeout(tenSeconds, 10000);
+setTimeout(timeUp, 15000);
+// ---------------------
+
+// Step 3:
+// Fill in the blanks to these functions.
+function fiveSeconds() {
+  console.log("10 seconds left");
+  document.getElementById("time-left").innerHTML =
+    "<h2>About 10 Seconds Left!</h2>";
+  // in the element with an id of `time-left` add an h2 saying About 10 Seconds Left!
+  // console log 10 seconds left
+}
+
+function tenSeconds() {
+  console.log("5 seconds left");
+  document.getElementById("time-left").innerHTML =
+    "<h2>About 5 Seconds Left!</h2>";
+  // in the element with an id of `time-left` add an h2 saying About 5 Seconds Left!
+  // console log 5 seconds left
+}
+
+function timeUp() {
+  console.log("done");
+  document.getElementById("time-left").innerHTML = "<h2>Time's Up!</h2>";
+  // in the element with an id of `time-left` add an h2 saying Time's Up!
+  // console log done
+
+  // The following line will play the audio file above
+  audio.play();
+}
