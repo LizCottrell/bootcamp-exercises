ddcb4642b394512aa5ab3ff490848a2f7ea77400
Elizabeth Cottrell <elizabeth@openforge.io>
2019-03-01 07:51:45 -0500

    ajax homework + 07-firebase unsolved

--- 06-ajax/homework/scripts.js
+++ 06-ajax/homework/scripts.js
@@ -1,65 +1,80 @@
-var apiKey = "cPHJBG5W7YeAMoUu72FUXO12yFNCVrsB;
-var queryURL = `http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=${apiKey}`;
+var apiKey = "cPHJBG5W7YeAMoUu72FUXO12yFNCVrsB";
 
-var searchTerm;
-var numberOfRecords;
-var beginDate;
-var endDate;
-var params;
+var topics = [
+  "Mommie Dearest",
+  "Carrie Fisher",
+  "Joan Crawford",
+  "Little Edie",
+  "Debbie Reynolds"
+];
 
-let resetForm = function() {
-  event.preventDefault();
-  searchTerm = "";
-  numberOfRecords = "";
-  beginDate = "";
-  endDate = "";
-  $("#formResults").html("");
-};
-
-$("#formSearch").on("click", function() {
-  event.preventDefault();
-  resetForm();
-
-  searchTerm = $("#formSearchTerm").val();
-  numberOfRecords = $("#formNumberOfRecords").val();
-  beginDate = $("#formStartYear").val();
-  endDate = $("#formEndYear").val();
-
-  if (beginDate !== "" && endDate !== "") {
-    params = `&q=${searchTerm}&begin_date=${beginDate}0101&end_date=${endDate}1231`;
-  } else if (beginDate !== "" && endDate === "") {
-    params = `&q=${searchTerm}&begin_date=${beginDate}0101`;
-  } else if (beginDate === "" && endDate !== "") {
-    params = `&q=${searchTerm}&end_date=${endDate}1231`;
-  } else {
-    params = `&q=${searchTerm}`;
-  }
-
-  console.log(queryURL + params);
+function displayGifs() {
+  var params = $(this).attr("data-name");
+  var queryURL = `http://api.giphy.com/v1/gifs/search?q=${params}&api_key=${apiKey}`;
 
   $.ajax({
-    url: queryURL + params,
+    url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    var result = response.response.docs;
-
-    for (let i = 0; i < numberOfRecords; i++) {
-      var newDiv = $(`
-        <div>
-          <h3>${result[i].headline.main}</h3>
-          <p>${result[i].byline.original}</p>
-          <p>Section: ${result[i].section_name}</p>
-          <p>${result[i].pub_date}</p>
-          <p><a href="${result[i].web_url}">${result[i].web_url}</a></p>
+    for (let i = 0; i < 10; i++) {
+      var gif = $(`
+      <button class="gif card btn btn-light m-4 d-flex justify-content-center" style="width: 18rem;">
+        <img 
+          src="${response.data[i].images.fixed_height_still.url}" 
+          data-still="${response.data[i].images.fixed_height_still.url}"
+          data-animate="${response.data[i].images.fixed_height.url}"
+          data-state="still" 
+          class="card-img-top" 
+          alt="..."
+        >
+        <div class="card-body">
+          <p class="card-text">Rating: ${response.data[i].rating}</p>
         </div>
-      `);
-
-      $("#formResults").append(newDiv);
+      </button>
+    `);
+      $("#gifs").prepend(gif);
     }
+
+    $(".gif").on("click", function() {
+      var state = $("img", this).attr("data-state");
+
+      if (state === "still") {
+        $("img", this).attr("src", $("img", this).attr("data-animate"));
+        $("img", this).attr("data-state", "animate");
+      } else {
+        $("img", this).attr("src", $("img", this).attr("data-still"));
+        $("img", this).attr("data-state", "still");
+      }
+    });
   });
+}
+
+function renderButtons() {
+  $("#buttons").empty();
+
+  for (var i = 0; i < topics.length; i++) {
+    var a = $(
+      `<button class='topic btn btn-secondary mr-3 mb-3' data-name='${
+        topics[i]
+      }'>
+        ${topics[i]}
+      </button>`
+    );
+    $("#buttons").append(a);
+  }
+}
+
+$("#add-topic").on("click", function(event) {
+  event.preventDefault();
+  var topic = $("#topic-input")
+    .val()
+    .trim();
+
+  topics.push(topic);
+
+  renderButtons();
 });
 
-$("#formClear").on("click", function() {
-  resetForm();
-});
+$(document).on("click", ".topic", displayGifs);
+renderButtons();
