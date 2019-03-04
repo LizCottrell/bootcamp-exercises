5af11c347f800afa7091bea53a041bff4726253d
Elizabeth Cottrell <elizabeth@openforge.io>
2019-03-04 07:34:00 -0500

    08-firebase mid-progress

--- 07-firebase/11-countdownbutton/Unsolved/logicOption2.js
+++ 07-firebase/11-countdownbutton/Unsolved/logicOption2.js
@@ -1,29 +1,44 @@
 // Initialize Firebase (YOUR OWN APP)
+var config = {
+  apiKey: "AIzaSyDKp9dV9ufQTSkjxjBbEMURJLQRDmr3vSg",
+  authDomain: "testproject-290fd.firebaseapp.com",
+  databaseURL: "https://testproject-290fd.firebaseio.com",
+  projectId: "testproject-290fd",
+  storageBucket: "testproject-290fd.appspot.com",
+  messagingSenderId: "540813625846"
+};
 
-// Set Initial Counter
-var initialValue = 100;
+firebase.initializeApp(config);
 
+var database = firebase.database();
+var initialValue = 100; // Set Initial Counter
 var clickCounter = initialValue;
 
 // --------------------------------------------------------------
 
 // At the initial load, get a snapshot of the current data.
-
+database.ref().on(
+  "value",
+  function(snapshot) {
+    console.log(snapshot.val());
+    clickCounter = snapshot.val().clickCount;
+    $("#click-value").text(snapshot.val().clickCount);
+  },
+  function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  }
+);
 
 // Print the initial data to the console.
 
-
 // Change the html to reflect the initial value.
-
+$("#click-value").text(clickCounter);
 
 // Change the clickCounter to match the data in the database
 
-
 // Log the value of the clickCounter
 
-
 // Change the HTML Value
-
 
 // If any errors are experienced, log them to console.
 
@@ -31,33 +46,25 @@ var clickCounter = initialValue;
 
 // Whenever a user clicks the click button
 $("#click-button").on("click", function() {
-
   // Reduce the clickCounter by 1
-
-
+  clickCounter--;
   // Alert User and reset the counter
-
-
   // Save new value to Firebase
-
-
+  database.ref().set({
+    clickCount: clickCounter
+  });
   // Log the value of clickCounter
-
-
 });
 
 // Whenever a user clicks the restart button
 $("#restart-button").on("click", function() {
-
   // Set the clickCounter back to initialValue
-
-
+  clickCounter = 100;
   // Save new value to Firebase
-
-
+  database.ref().set({
+    clickCount: clickCounter
+  });
   // Log the value of clickCounter
-
-
   // Change the HTML Values
-
+  $("#click-value").text(clickCounter);
 });
--- 07-firebase/14-codersbay/Unsolved/logicOption2.js
+++ 07-firebase/14-codersbay/Unsolved/logicOption2.js
@@ -1,10 +1,16 @@
 // Initialize Firebase
-// Make sure to match the configuration to the script version number in the HTML
-// (Ex. 3.0 != 3.7.0)
+var config = {
+  apiKey: "AIzaSyDKp9dV9ufQTSkjxjBbEMURJLQRDmr3vSg",
+  authDomain: "testproject-290fd.firebaseapp.com",
+  databaseURL: "https://testproject-290fd.firebaseio.com",
+  projectId: "testproject-290fd",
+  storageBucket: "testproject-290fd.appspot.com",
+  messagingSenderId: "540813625846"
+};
 
-// Assign the reference to the database to a variable named 'database'
-// var database = ...
+firebase.initializeApp(config);
 
+var database = firebase.database();
 
 // Initial Values
 var initialBid = 0;
@@ -16,60 +22,77 @@ var highBidder = initialBidder;
 
 //  At the page load and subsequent value changes, get a snapshot of the stored data.
 // This function allows you to update your page in real-time when the firebase database changes.
-
-
 // If Firebase has a highPrice and highBidder stored (first case)
-
-
 // Set the variables for highBidder/highPrice equal to the stored values in firebase.
 // highPrice = ...
 // highBidder = ...
-
-
 // Change the HTML to reflect the stored values
-
-
 // Print the data to the console.
-
-
 // Else Firebase doesn't have a highPrice/highBidder, so use the initial local values.
-
-
 // Change the HTML to reflect the initial values
-
-
 // Print the data to the console.
 
-
-
+database.ref().on(
+  "value",
+  function(snapshot) {
+    console.log(snapshot.val());
+    if (snapshot.val().length > 0) {
+      highPrice = snapshot.val().highPrice;
+      highBidder = snapshot.val().highBidder;
+      $("#highest-bidder").text(snapshot.val().name);
+      $("#highest-price").text(snapshot.val().price);
+    } else {
+      $("#highest-bidder").text(initialBidder);
+      $("#highest-price").text(initialBid);
+    }
+  },
+  function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  }
+);
 
 // --------------------------------------------------------------
 
 // Whenever a user clicks the submit-bid button
-
 // prevent form from submitting with event.preventDefault() or returning false
-
 // Get the input values
-
-
 // Log the Bidder and Price (Even if not the highest)
-
-
 // If Then statements to compare against previous high bidder
-
-
 // Alert that they are High Bidder
-
-
 // Save the new price in Firebase
-
-
 // Log the new High Price
-
-
 // Store the new high price and bidder name as a local variable (could have also used the firebase variable)
-
-
 // Change the HTML to reflect the new high price and bidder
-
 // Else tell user their bid was too low via alert
+
+$("#submit-bid").on("click", function(event) {
+  event.preventDefault();
+  name = $("#bidder-name")
+    .val()
+    .trim();
+  price = $("#bidder-price")
+    .val()
+    .trim();
+  console.log("highPrice: ", highPrice);
+  console.log("highBidder: ", highBidder);
+  if (highPrice > price) {
+    alert("Your bid was too low. Try again.");
+  } else {
+    alert("You won! You're now the high bidder.");
+    database.ref().set({
+      name: name,
+      price: price
+    });
+
+    database.ref().on(
+      "value",
+      function(snapshot) {
+        $("#highest-bidder").text(snapshot.val().name);
+        $("#highest-price").text(snapshot.val().price);
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      }
+    );
+  }
+});
